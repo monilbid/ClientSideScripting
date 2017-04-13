690a56b3dac1e8ccd8727e22eb7974f9c0518971
Monil Bid <monilbid60@gmail.com>
2017-04-13 11:51:43 -0400

    Added Sorting functionality to the Novel List.

--- web/tripleLab/js/getBestSellerNovelsList.js
+++ web/tripleLab/js/getBestSellerNovelsList.js
@@ -4,12 +4,20 @@ myapp.controller('BestSellerBooksCtrl', function($scope, $http){
         console.log(response);
         console.log("");
         $scope.books = response.data.recordList;
+        
+        for(var i = 0; i < $scope.books.length; i++){
+            $scope.books[i].num_id = Number($scope.books[i].id);
+            $scope.books[i].num_year_published = parseInt($scope.books[i].year_published.replace(/\,/g,''));
+            if($scope.books[i].price){
+                $scope.books[i].num_price = parseFloat($scope.books[i].price.replace(/\$/g, ''));
+            }
+        }
     }, function(response){
         console.log("AJAX Call Failed while Getting Books List");
         console.log(response);
         console.log("");
     });
     
-    $scope.sortField = 'id';
+    $scope.sortField = 'num_id';
     $scope.reverse = true;
 });
